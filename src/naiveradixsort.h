50187284284747e6d8aedb268f3cff65f8f2afb8
Kirill Lykov <klykov1@bloomberg.net>
2021-04-12 10:32:56 +0200

    move hist out

--- src/naiveradixsort.h
+++ src/naiveradixsort.h
@@ -1,19 +1,24 @@
 #pragma once
 #include <array>
+
 #include <vector>
 
-constexpr int npart = 4;
+constexpr size_t radix_bits  = 8; // 8 gives x5 speed up over 2
+constexpr size_t radix_size = 1ull << radix_bits;
+constexpr size_t radix_levels = 63ull/radix_bits + 1;
+constexpr size_t radix_mask = radix_size - 1;
+
 using T = int64_t;
 
 // This one is faster
 inline void partition_backward(const std::vector<T>& data, std::function<int(T)> partFunc, std::vector<T>& out)
 {
-    std::array<int, npart> psum = { 0 };
+    std::array<int, radix_size> psum = { 0 };
     //count sort
     for (T v : data) {
         ++psum[partFunc(v)];
     }
-    for (int i = 1; i < npart; ++i)
+    for (int i = 1; i < radix_size; ++i)
         psum[i] += psum[i - 1];
     int n = (int)data.size();
     for (int j = n - 1; j >= 0; --j) {
@@ -23,18 +28,49 @@ inline void partition_backward(const std::vector<T>& data, std::function<int(T)>
     }
 }
 
+void radixSort_count(std::vector<T>& data, std::vector<T>& buf)
+{
+    const int n = data.size();
+    constexpr auto sz = sizeof(T) * 8 - 2;
+    constexpr auto partFunc = [](T v, size_t i) -> int { return (v >> i) & radix_mask; };
+
+    // this moving out of loop gives 50%
+    size_t hist[radix_levels][radix_size] = {};
+    for (T shift = 0, it = 0; shift < sz; shift += radix_bits, ++it) {
+        // it == shift / radix_bits but it is easier to have it here
+        for (T v : data) {
+            ++hist[it][partFunc(v, shift)];
+        }
+    }
+
+    for (T shift = 0, it = 0; shift < sz; shift += radix_bits, ++it) {
+        std::array<int, radix_size> psum = { 0 }; // just moving this out of the loop doesn't give any speed up
+        psum[0] = hist[it][0];
+        for (int i = 1; i < radix_size; ++i)
+            psum[i] = psum[i - 1] + hist[it][i];
+        int n = (int)data.size();
+        for (int j = n - 1; j >= 0; --j) {
+            auto bits = partFunc(data[j], shift);
+            --psum[bits];
+            buf[psum[bits]] = data[j];
+        }
+        swap(buf, data);
+    }
+}
+
+// Slower
 inline void partition_forward(const std::vector<T>& t_in, std::function<int(T)> partFunc, std::vector<T>& t_out)
 {
     /// histogram
-    std::array<int, npart> histogram = {};
+    std::array<int, radix_size> histogram = {};
     for (T v : t_in) {
         ++histogram[partFunc(v)];
     }
 
     /// partitioning
-    std::array<int, npart> offset;
+    std::array<int, radix_size> offset;
     int i = 0;
-    for (int p = 0; p < npart; ++p) {
+    for (int p = 0; p < radix_size; ++p) {
         offset[p] = i;
         i += histogram[p];
     }
@@ -43,16 +79,5 @@ inline void partition_forward(const std::vector<T>& t_in, std::function<int(T)> 
         int i_out = offset[partFunc(key)];
         ++offset[partFunc(key)];
         t_out[i_out] = key;
-    }
-}
-
-void radixSort_count(std::vector<T>& data, std::vector<T>& buf)
-{
-    const int n = data.size();
-    constexpr auto sz = sizeof(T) * 8 - 2;
-    for (T i = 0; i < sz; i += 2) {
-        partition_backward(
-            data, [i](T v) -> int { return (v >> i) & 0b11; }, buf);
-        swap(buf, data);
     }
 }
